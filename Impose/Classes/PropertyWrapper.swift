e1ad7260d343fe961808d8297e585f26f9d1861a
Nayanda Haberty <hainayanda@outlook.com>
2022-03-23 22:48:54 +0700

    Added scoped injector in property wrapper

--- Impose/Classes/PropertyWrapper.swift
+++ Impose/Classes/PropertyWrapper.swift
@@ -16,27 +16,45 @@ public class Injected<T> {
         return result.value
     }()
     
-    let rules: InjectionRules
-    let type: ImposerType
     lazy var context: ImposeContext? = {
         let result = resolveAndGetContext()
         self.wrappedValue = result.value
         return result.context
     }()
     
+    var injector: Injector {
+        scopedInjector ?? Injector.shared
+    }
+    let scopedInjector: Injector?
+    
+    let rules: InjectionRules
+    let type: ImposerType
+    
     @available(*, deprecated, message: "Use no param instead, will be removed in next release")
     public init(type: ImposerType) {
+        self.scopedInjector = nil
         self.rules = .nearest
         self.type = type
     }
     
     @available(*, deprecated, message: "Use no param instead, will be removed in next release")
     public init(ifNoMatchUse rules: InjectionRules = .nearest) {
+        self.scopedInjector = nil
         self.rules = rules
         self.type = .primary
     }
     
+    /// Default init
     public init() {
+        self.scopedInjector = nil
+        self.rules = .nearest
+        self.type = .primary
+    }
+    
+    /// Init with given injector
+    /// - Parameter injector: injector used to inject
+    public init(scopedBy injector: Injector) {
+        self.scopedInjector = injector
         self.rules = .nearest
         self.type = .primary
     }
@@ -47,8 +65,8 @@ public class Injected<T> {
     
     func resolveAndGetContext() -> (value: T, context: ImposeContext?){
         do {
-            let value = try tryInject(T.self)
-            let context = Injector.shared.context(of: T.self)
+            let value = try injector.resolve(T.self)
+            let context = injector.context(of: T.self)
             return (value, context)
         } catch {
             // will be removed on next release
@@ -86,16 +104,29 @@ public class SafelyInjected<T> {
         return result.context
     }()
     
+    var injector: Injector {
+        scopedInjector ?? Injector.shared
+    }
+    let scopedInjector: Injector?
+    
     /// Default init
-    public init() { }
+    public init() {
+        self.scopedInjector = nil
+    }
+    
+    /// Init with given injector
+    /// - Parameter injector: injector used to inject
+    public init(scopedBy injector: Injector) {
+        self.scopedInjector = injector
+    }
     
     public var projectedValue: ImposeContext? {
         context
     }
     
     func resolveAndGetContext() -> (value: T?, context: ImposeContext?){
-        let value = injectIfProvided(for: T.self)
-        let context = Injector.shared.context(of: T.self)
+        let value = try? injector.resolve(T.self)
+        let context = injector.context(of: T.self)
         return (value, context)
     }
 }
