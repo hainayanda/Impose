b508a498dd7d31c127df9a44fc0a41cd50fcae53
Nayanda Haberty <hainayanda@outlook.com>
2022-03-23 21:20:40 +0700

    Update Nimble

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", from: "4.0.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.0")
+        .package(url: "https://github.com/Quick/Nimble.git", from: "9.2.1")
     ],
     targets: [
         .target(
