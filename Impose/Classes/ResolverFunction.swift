ea6921f454f591ee486529363bda29999da02c9c
Nayanda Haberty <hainayanda@outlook.com>
2022-03-23 22:52:18 +0700

    Add scoped injector to resolve function

--- Impose/Classes/ResolverFunction.swift
+++ Impose/Classes/ResolverFunction.swift
@@ -8,21 +8,24 @@
 import Foundation
 
 /// get instance of the given type. It will throw ImposeError if fail
-/// - Parameter type: type of instance
+/// - Parameters:
+///   - type: type of instance
+///   - injector: Injector that will resolve the instance
 /// - Throws: ImposeError
 /// - Returns: instance resolved
-public func tryInject<T>(_ type: T.Type) throws -> T {
-    try Injector.shared.resolve(type)
+public func tryInject<T>(_ type: T.Type, scopedBy injector: Injector = Injector.shared) throws -> T {
+    try injector.resolve(type)
 }
 
 /// get instance of the given type. It will use given closure if fail
 /// - Parameters:
 ///   - type: type of instance
 ///   - resolve: closure resolver to call if inject fail
+///   - injector: Injector that will resolve the instance
 /// - Returns:  instance resolved
-public func inject<T>(_ type: T.Type, ifFail resolve: () -> T) -> T {
+public func inject<T>(_ type: T.Type, scopedBy injector: Injector = Injector.shared, ifFail resolve: () -> T) -> T {
     do {
-        return try Injector.shared.resolve(type)
+        return try injector.resolve(type)
     } catch {
         return resolve()
     }
@@ -31,26 +34,31 @@ public func inject<T>(_ type: T.Type, ifFail resolve: () -> T) -> T {
 /// get instance of the given type. It will use given autoclosure if fail
 /// - Parameters:
 ///   - type: type of instance
+///   - injector: Injector that will resolve the instance
 ///   - resolve: autoclosure resolver to call if inject fail
 /// - Returns:  instance resolved
-public func inject<T>(_ type: T.Type, ifFailUse resolve: @autoclosure () -> T) -> T {
+public func inject<T>(_ type: T.Type, scopedBy injector: Injector = Injector.shared, ifFailUse resolve: @autoclosure () -> T) -> T {
     do {
-        return try Injector.shared.resolve(type)
+        return try injector.resolve(type)
     } catch {
         return resolve()
     }
 }
 
 /// get instance of the given type. It will throws fatal error if fail
-/// - Parameter type: type of instance
+/// - Parameters:
+///   - type: type of instance
+///   - injector: Injector that will resolve the instance
 /// - Returns: instance resolved
-public func inject<T>(_ type: T.Type) -> T {
-    try! tryInject(type)
+public func inject<T>(_ type: T.Type, scopedBy injector: Injector = Injector.shared) -> T {
+    try! tryInject(type, scopedBy: injector)
 }
 
 /// get instance of the given type. it will return nil if fail
-/// - Parameter type: type of instance
+/// - Parameters:
+///   - type: type of instance
+///   - injector: Injector that will resolve the instance
 /// - Returns: instance resolved if found and nil if not
-public func injectIfProvided<T>(for type: T.Type) -> T? {
-    try? tryInject(type)
+public func injectIfProvided<T>(for type: T.Type, scopedBy injector: Injector = Injector.shared) -> T? {
+    try? tryInject(type, scopedBy: injector)
 }
