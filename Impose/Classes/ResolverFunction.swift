b16b2b199ee117b0857058d4df1334b37d173c1c
Nayanda Haberty <hainayanda@outlook.com>
2022-03-23 22:04:26 +0700

    Add documentation

--- Impose/Classes/ResolverFunction.swift
+++ Impose/Classes/ResolverFunction.swift
@@ -15,6 +15,11 @@ public func tryInject<T>(_ type: T.Type) throws -> T {
     try Injector.shared.resolve(type)
 }
 
+/// get instance of the given type. It will use given closure if fail
+/// - Parameters:
+///   - type: type of instance
+///   - resolve: closure resolver to call if inject fail
+/// - Returns:  instance resolved
 public func inject<T>(_ type: T.Type, ifFail resolve: () -> T) -> T {
     do {
         return try Injector.shared.resolve(type)
@@ -23,6 +28,11 @@ public func inject<T>(_ type: T.Type, ifFail resolve: () -> T) -> T {
     }
 }
 
+/// get instance of the given type. It will use given autoclosure if fail
+/// - Parameters:
+///   - type: type of instance
+///   - resolve: autoclosure resolver to call if inject fail
+/// - Returns:  instance resolved
 public func inject<T>(_ type: T.Type, ifFailUse resolve: @autoclosure () -> T) -> T {
     do {
         return try Injector.shared.resolve(type)
