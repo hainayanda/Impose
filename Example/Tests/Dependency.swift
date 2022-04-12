085273a39cc2ce8d4c5f0d9f47429d7541a23cdc
Nayanda Haberty <hainayanda@outlook.com>
2022-04-12 14:51:18 +0700

    Update unit test

--- Example/Tests/Dependency.swift
+++ Example/Tests/Dependency.swift
@@ -9,22 +9,37 @@
 import Foundation
 
 protocol Dependency: AnyObject {
+    
+    var creationCount: Int { get }
     func explainMyself() -> String
 }
 
+
 class ChildDependency: Dependency {
+    
+    static var objectCreationCounter: Int = 0
+    
+    let creationCount: Int
+    
+    init() {
+        ChildDependency.objectCreationCounter += 1
+        creationCount = ChildDependency.objectCreationCounter
+    }
+    
     func explainMyself() -> String {
         return "I am ChildDependency and Injected"
     }
 }
 
 class GrandChildDependency: ChildDependency {
+    
     override func explainMyself() -> String {
         return "I am GrandChildDependency and Injected"
     }
 }
 
 class GrandGrandChildDependency: GrandChildDependency {
+    
     override func explainMyself() -> String {
         return "I am SomeOtherUpperDependency and Injected"
     }
